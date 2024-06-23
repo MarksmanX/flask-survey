d59fccd2aaaa96a7633810417c56b1b060733410
MarksmanX <mkeph561@gmail.com>
2024-06-23 17:05:29 -0400

    Finished further study

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, redirect, flash, request, session
+from flask import Flask, render_template, redirect, flash, request, session, make_response
 from flask_debugtoolbar import DebugToolbarExtension
-from surveys import satisfaction_survey as survey
+from surveys import surveys
 
+CURRENT_SURVEY_KEY = 'current-survey'
 RESPONSES_KEY = "responses"
 
 app = Flask(__name__)
@@ -13,13 +14,27 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
 @app.route('/')
-def show_startpage():
+def show_survey_pick_form():
+    """Show pick-a-survey form."""
+
+    return render_template('startpage.html', surveys=surveys)
+
+
+@app.route("/", methods=["POST"])
+def pick_survey():
     """Select a survey."""
 
-    return render_template('startpage.html', survey=survey)
+    survey_id = request.form['survey_code']
+    survey = surveys[survey_id]
+    session[CURRENT_SURVEY_KEY] = survey_id
 
+    # don't let them re-take a survey until cookie times out
+    if request.cookies.get(f"completed_{survey_id}"):
+        return render_template("already-done.html", survey=survey)
 
-@app.route('/begin')
+    return render_template("survey-start.html", survey=survey)
+
+@app.route('/begin', methods=["POST"])
 def begin_survey():
     """Clear the session of responses"""
 
@@ -32,6 +47,8 @@ def begin_survey():
 def show_question(qid):
     """Display current question"""
     responses = session.get(RESPONSES_KEY)
+    survey_code = session[CURRENT_SURVEY_KEY]
+    survey = surveys[survey_code]
 
     if (responses is None):
         # trying to access question page too soon
@@ -47,6 +64,7 @@ def show_question(qid):
         return redirect(f"/questions/{len(responses)}")
     
     question = survey.questions[qid]
+
     return render_template("question.html",
                            question_num=qid,
                            question=question)
@@ -58,11 +76,16 @@ def handle_question():
 
     #get the response choice
     choice = request.form['answer']
+    text = request.form.get("text", "")
 
     #add this response to the session
     responses = session[RESPONSES_KEY]
-    responses.append(choice)
+    responses.append({"choice": choice, "text": text})
+
+    # add this response to the session
     session[RESPONSES_KEY] = responses
+    survey_code = session[CURRENT_SURVEY_KEY]
+    survey = surveys[survey_code]
 
     if (len(responses) == len(survey.questions)):
         #They've answered all the questions! Thank them.
@@ -73,6 +96,17 @@ def handle_question():
 
 @app.route("/complete")
 def complete():
-    """Survey complete. Show completion page."""
+    """Thank user and list responses"""
 
-    return render_template("completion.html")
+    survey_id = session[CURRENT_SURVEY_KEY]
+    survey = surveys[survey_id]
+    responses = session[RESPONSES_KEY]
+
+    html = render_template("completion.html",
+                           survey=survey,
+                           responses=responses)
+
+    # Set cookie noting this survey is dones so they can't re-do it
+    response = make_response(html)
+    response.set_cookie(f"completed_{survey_id}", "yes", max_age=60)
+    return response
